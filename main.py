a8ec82b597ac73e49b0d1bc1f82f04754964331b
AlexAmanitas <104567917+AlexAmanitas@users.noreply.github.com>
2023-01-20 23:16:44 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,45 +1,127 @@
 import pygame 
-from pygame.constants import QUIT
+from pygame.constants import QUIT, K_DOWN, K_UP, K_LEFT, K_RIGHT
 import random
 
 pygame.init()
 
+FPS = pygame.time.Clock()
+
 RED = 255,0,0
 BLACK = 0,0,0
+WHITE = 255,255,255
+BLUE = 0,130,209
+YELLOW = 255,209,0
 
 screen = width, height= 800, 600
 
 main_surface =  pygame.display.set_mode(screen)
 
 ball = pygame.Surface((20, 20))
-ball.fill(RED)
+ball.fill(WHITE)
 ball_rect = ball.get_rect()
-ball_speed = [1, 1]
+ball_speed = 5
+
+def create_enemy():
+    enemy = pygame.Surface((20,20))
+    enemy.fill(RED)
+    enemy_rect = pygame.Rect(width, random.randint(0,height), *enemy.get_size())
+    enemy_speed = random.randint(2,5)
+    return [enemy, enemy_rect, enemy_speed]
+
+CREATE_ENEMY = pygame.USEREVENT+1
+pygame.time.set_timer(CREATE_ENEMY, 2500)
+
+enemies = []
+
+def create_bonus():
+    bonus = pygame.Surface((20,20))
+    bonus1 = pygame.Surface((20,10))
+    bonus1.fill(YELLOW)
+    bonus.fill(BLUE)
+    bonus.blit(bonus1,(0,10))
+    bonus_rect = pygame.Rect(random.randint(0,width), 0, *bonus.get_size())
+    bonus_speed = random.randint(2,4)
+    return [bonus, bonus_rect, bonus_speed]
+
+CREATE_BONUS = pygame.USEREVENT+1
+pygame.time.set_timer(CREATE_BONUS, 1000)
+
+bonuses = []
 
 is_working = True
 
 while is_working:
-   for event in pygame.event.get():
-      if event.type == QUIT:
-        is_working = False
 
-   ball_rect = ball_rect.move(ball_speed) 
+    FPS.tick(60)
 
-   if ball_rect.bottom >= height or ball_rect.top <= 0:
-      red = random.randint(0,255)
-      green = random.randint(0,255)
-      blue = random.randint(0,255)
-      ball_speed[1] = -ball_speed[1]
-      ball.fill((red, green, blue))
+    for event in pygame.event.get():
+        if event.type == QUIT:
+             is_working = False
+
+        if event.type == CREATE_ENEMY:
+            enemies.append(create_enemy())
+
+        if event.type == CREATE_BONUS:
+            bonuses.append(create_bonus())
+
+    pressed_keys = pygame.key.get_pressed()
+
+    main_surface.fill(BLACK)    
+
+    main_surface.blit(ball, ball_rect)	
+
+    for enemy in enemies:
+        enemy[1] = enemy[1].move(-enemy[2],0)
+        main_surface.blit(enemy[0], enemy[1])	
+
+        if enemy[1].left < 0:
+            enemies.pop(enemies.index(enemy))
+
+        if ball_rect.colliderect(enemy[1]):
+            enemies.pop(enemies.index(enemy))
+
+    for bonus in bonuses:
+        bonus[1] = bonus[1].move(0,bonus[2])
+        main_surface.blit(bonus[0], bonus[1])	
+
+        if bonus[1].bottom > width:
+            bonuses.pop(bonuses.index(bonus))
+
+        if ball_rect.colliderect(bonus[1]):
+            bonuses.pop(bonuses.index(bonus))
+
+
+    if pressed_keys[K_DOWN] and not ball_rect.bottom >= height:
+        ball_rect = ball_rect.move(0, ball_speed)
+
+    if pressed_keys[K_UP] and not ball_rect.top <= 0:
+        ball_rect = ball_rect.move(0, -ball_speed)
+
+    if pressed_keys[K_RIGHT] and not ball_rect.right >= width:
+        ball_rect = ball_rect.move( ball_speed,0)
+
+    if pressed_keys[K_LEFT] and not ball_rect.left <= 0:
+        ball_rect = ball_rect.move( -ball_speed,0)
+
+    pygame.display.flip()
+
+
+
+ # enemy_rect = enemy_rect.move(-enemy_speed,0)
+
+    # print(len(bonuses))
+
+    # ball_rect = ball_rect.move(ball_speed) 
+  #  if ball_rect.bottom >= height or ball_rect.top <= 0:
+  #     red = random.randint(0,255)
+  #     green = random.randint(0,255)
+  #     blue = random.randint(0,255)
+  #     ball_speed[1] = -ball_speed[1]
+  #     ball.fill((red, green, blue))
       
-   if ball_rect.right >= width or ball_rect.left <= 0:
-      red = random.randint(0,255)
-      green = random.randint(0,255)
-      blue = random.randint(0,255)
-      ball_speed[0] = -ball_speed[0]
-      ball.fill((red, green, blue))
-
-   main_surface.fill(BLACK)    
-   main_surface.blit(ball, ball_rect)		
-   
-   pygame.display.flip()
+  #  if ball_rect.right >= width or ball_rect.left <= 0:
+  #     red = random.randint(0,255)
+  #     green = random.randint(0,255)
+  #     blue = random.randint(0,255)
+  #     ball_speed[0] = -ball_speed[0]
+  #     ball.fill((red, green, blue))
